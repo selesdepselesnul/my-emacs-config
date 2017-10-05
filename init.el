9c04246c19cd0c7522b013f8c525cf1d13c25f5c
selesdepselesnul <selesdepselesnul@gmail.com>
2017-10-06 05:33:57 +0700

    add autocomplete

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (request ## cider elpy))))
+ '(package-selected-packages (quote (ac-cider request ## cider elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,8 +20,10 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; turn off annoying bell
 (setq visible-bell t)
 
+;; paredit
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -29,4 +31,9 @@
 (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;; auto-complete
+(ac-config-default)
+
+;; line-number
 (global-linum-mode t)
