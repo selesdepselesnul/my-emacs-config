db368e6e278aa6127c93366e07db2d66d08a7155
selesdepselesnul <selesdepselesnul@gmail.com>
2017-10-06 05:14:38 +0700

    fix typo

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (cider elpy))))
+ '(package-selected-packages (quote (request ## cider elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
