c971d24f011553970ad1e756c4da26bee3c76559
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-04 18:42:17 +0700

    add magit

--- init.el
+++ init.el
@@ -190,6 +190,9 @@
 (use-package geiser
   :ensure t)
 
+(use-package magit
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
