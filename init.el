4311599c02afb88638a5b2f326f18952f36b0d18
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-09 20:08:46 +0700

    add flycheck

--- init.el
+++ init.el
@@ -206,6 +206,10 @@
 (use-package mmm-mode
   :ensure t)
 
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
