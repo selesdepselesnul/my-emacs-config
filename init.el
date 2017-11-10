edf13f89a2a2ff6a1e8da2f833a092ac63da9091
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-10 15:52:07 +0700

    delete mouse binding for cursor mark in multiple-cursors

--- init.el
+++ init.el
@@ -132,8 +132,7 @@
   :ensure t
   :bind (("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)
-         ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
+         ("C-c C-<" . mc/mark-all-like-this)))
 
 (use-package projectile
   :ensure t
