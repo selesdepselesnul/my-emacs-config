2efdd922ce6fd8355e0204c3d5176ea29c566920
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-20 13:31:34 +0700

    delete powerline

--- init.el
+++ init.el
@@ -208,11 +208,6 @@
   :ensure t
   :init (global-flycheck-mode))
 
-(use-package powerline
-  :ensure t
-  :config
-  (powerline-default-theme))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
